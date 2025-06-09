64d1c875e210df3ce71394e12b6a8a58090232d6
Stanislav Kelemen <kelemen.work@gmail.com>
2025-06-09 22:37:18 +0200

    init db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+
+const {
+  DB_HOST: host,
+  DB_PORT: port,
+  DB_USER: username,
+  DB_PASSWORD: password,
+  DB_NAME: database,
+} = process.env;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host,
+      port: parseInt(port || '5432', 10),
+      username,
+      password,
+      database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
